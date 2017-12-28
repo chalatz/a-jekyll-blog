77d55b89e26ad338ca5bbfb5520c6396c4208bf9
chalatz <chralatz@gmail.com>
2017-12-28 21:35:39 +0200

    Rebased master

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    pleeease = require('gulp-pleeease'),
+    wait = require('gulp-wait');
+
+gulp.task('default', ['serve']);
+
+gulp.task('build-css', function(){
+    return gulp.src('scss/main.scss')
+        .pipe(wait(50))
+        .pipe(sass({includePaths: ['./scss']}))
+        .pipe(pleeease({
+            "autoprefixer": {"browsers": ["last 4 versions", "ios 6"]},
+            "rem": false,
+            "minifier": false
+        }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve',['build-css'], function(){
+    browserSync.init({
+        server: {
+            baseDir: ''
+        }
+    });
+
+    gulp.watch('scss/**/*.scss', ['build-css']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// gulp.task('watch', function(){
+//     gulp.watch('app/js/**/*.js', ['jshint']);
+//     gulp.watch('app/scss/**/*.scss', ['build-css']);
+// });
